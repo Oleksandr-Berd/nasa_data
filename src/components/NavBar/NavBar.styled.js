423a33bc2e8f979caaa84bb673e9255f5f449074
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-12-28 12:33:42 +0200

    1

--- src/components/NavBar/NavBar.styled.js
+++ src/components/NavBar/NavBar.styled.js
@@ -4,4 +4,10 @@ export const NavLinkStyled = styled.span`
   color: whitesmoke;
   font-size: 30px;
   margin-right: 30px;
+  cursor: pointer;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  :hover,
+  :focus {
+    color: lightblue;
+  }
 `;
